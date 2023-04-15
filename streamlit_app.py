1dcee24b9ccfc1b8ff1862fa5cd4ebb4e2965799
Tursunali Kholdorov <79641787+Tursunali-portfolio@users.noreply.github.com>
2023-04-15 15:15:20 +0500

    Update streamlit_app.py
    organize imports

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
+import pandas as pd
+import requests
 import streamlit
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title("My Parents New Healthy Diner")
 
@@ -12,7 +15,6 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -24,8 +26,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-
-import requests
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
