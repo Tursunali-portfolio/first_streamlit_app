5092d00de1605939350d97fa1cb2e663e6c5995b
Tursunali Kholdorov <79641787+Tursunali-portfolio@users.noreply.github.com>
2023-04-25 16:27:12 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,6 @@ sweatsuit_list = data.set_index('COLOR_OR_STYLE')
 
 streamlit.dataframe(sweatsuit_list.index)
 
-selected_suits = streamlit.selectbox('Pick a sweatsuit color or style:', sweatsuit_list.index)
+selected_suit = streamlit.selectbox('Pick a sweatsuit color or style:', sweatsuit_list.index)
 
-# streamlit.dataframe(sweatsuit_list.loc[selected_suits])
+streamlit.text(selected_suit)
