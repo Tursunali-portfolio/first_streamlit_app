39b9c63af8311da20121fa0b169866759c28bf18
Tursunali Kholdorov <79641787+Tursunali-portfolio@users.noreply.github.com>
2023-04-25 16:07:41 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+from PIL import Image
+import requests
+from io import BytesIO
+
+import snowflake.connector
+import streamlit
+
+
+conn = snowflake.connector.connect(**streamlit.secrets["snowflake_zena"])
+
+with conn.cursor() as cursor:
+  cursor.execute("SELECT * FROM ZENAS_ATHLEISURE_DB.PRODUCTS.CATALOG_FOR_WEBSITE;")
+  data = cursor.fetch_pandas_all()
+
+sweatsuit_list = data.set_index('COLOR_OR_STYLE')
+
+selected_suits = streamlit.selectbox('Pick a sweatsuit color or style:', list(sweatsuit_list), ["Pink"])
+
+streamlit.dataframe(sweatsuit_list.loc[selected_suits])
